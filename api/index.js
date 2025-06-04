25297282b66f5f64d2f6bf51f1b738432a35b1ed
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-06-04 10:42:59 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,8 +9,7 @@ const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-	credential;
-admin.credential.cert(serviceAccount)
+	credential:admin.credential.cert(serviceAccount)
 });
 
 const app = express();
