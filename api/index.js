e7d400d36979a315bac04f895977a8275a2d72af
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-06-04 10:56:17 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const db = require('./firebase-config');
+const db = require('./../firebase-config');
 require("serverless-http");
 
 const app = express();
